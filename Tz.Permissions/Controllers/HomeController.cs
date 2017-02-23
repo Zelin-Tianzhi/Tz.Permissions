869b6871b54d9b6acd45f688e24cc6de22599305
Zelin-Tianzhi <walkingfy@gmail.com>
2017-02-24 00:13:37 +0800

    提交测试

--- Tz.Permissions/Controllers/HomeController.cs
+++ Tz.Permissions/Controllers/HomeController.cs
@@ -17,6 +17,7 @@ namespace Tz.Permissions.Controllers
 			ViewData["Version"] = mvcName.Version.Major + "." + mvcName.Version.Minor;
 			ViewData["Runtime"] = isMono ? "Mono" : ".NET";
 
+
 			return View();
 		}
 	}
